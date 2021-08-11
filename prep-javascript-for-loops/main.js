a2660b99a8dd1c9040044525d37399b75273126a
Aaron Woo <awoo@chapman.edu>
2021-08-11 12:07:51 -0700

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log('For loop incremented by 2:');
+for (let i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+console.log('Countdown:');
+for (let i = 100; i > -1; i--) {
+  console.log('Time till explosion ' + i + '!');
+}
